58fb437b8bbd67b902c3be8cbb160e69fd7ff55c
ricardohtduarte <ricardohtduarte@gmail.com>
2018-04-19 15:47:39 +0100

    Launch animation complete

---
+++ MinderaUI/MinderaUI/LaunchVC.swift
@@ -0,0 +1,32 @@
+//
+//  LaunchVC.swift
+//  MinderaUI
+//
+//  Created by Ricardo Duarte on 19/04/2018.
+//  Copyright © 2018 Ricardo Duarte. All rights reserved.
+//
+
+import UIKit
+
+class LaunchVC: UIViewController {
+    
+    @IBOutlet weak var logo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animate_logo()
+    }
+    
+    func animate_logo(){
+        UIView.animate(withDuration: 1.5, animations: {
+            self.logo.transform = CGAffineTransform(rotationAngle: CGFloat.pi) },
+                       completion: { finished in
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            let home = self.storyboard?.instantiateViewController(withIdentifier: "home")
+                            self.present(home!, animated: true, completion: nil)
+                        }
+                        })
+        }
+}
+
+
