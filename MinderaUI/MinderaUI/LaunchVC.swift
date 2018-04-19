5aa8a7efd11877731eba4f20f36ef7ab2de41092
ricardohtduarte <ricardohtduarte@gmail.com>
2018-04-19 15:57:40 +0100

    Minor changes

--- MinderaUI/MinderaUI/LaunchVC.swift
+++ MinderaUI/MinderaUI/LaunchVC.swift
@@ -19,9 +19,11 @@ class LaunchVC: UIViewController {
     
     func animate_logo(){
         UIView.animate(withDuration: 1.5, animations: {
+            //180 degrees animation
             self.logo.transform = CGAffineTransform(rotationAngle: CGFloat.pi) },
                        completion: { finished in
                         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            //initiate navigation controller
                             let home = self.storyboard?.instantiateViewController(withIdentifier: "home")
                             self.present(home!, animated: true, completion: nil)
                         }
